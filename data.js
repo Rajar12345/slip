a4026e1cea8ea02ef53a93222bfc411246298f85
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2023-10-31 10:47:33 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var userDatabase = [
-        [ 'L123443', 'Hyderabad ', 'Travel', 'Vacation', '20-Dec-2023 5:00 am', '27-Dec-2023 5:00 am', '',],
+        [ 'L123443', 'Hyderabad ', 'Travel', 'Vacation', '20-Dec-2023 5:00 am', '27-Dec-2023 5:00 am', 'Approved',],
         [ 'L123786', 'Chennai', 'Visiting Home', 'Vacation', '10-May-2023 9:00 am', '17-June-2023 11:00 am', ''],
         [ 'L123890', 'Chennai', 'Visiting Home', 'Vacation', '07-Mar-2023 9:30 am', '09-Mar-2023 7:30 pm', ''],
         [ 'L165456', 'Bengaluru', 'Travel', 'Vacation', '05-Nov-2023 5:30 am', '17-Nov-2023 6:00 am', ''],
